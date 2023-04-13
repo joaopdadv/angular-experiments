2016335e409277e90312c826ded6e3275417202b
joaopdadv <joaopdadv@gmail.com>
2023-04-13 11:07:33 -0300

    inicio login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +23,7 @@ import { PokerPageComponent } from './poker/poker-page/poker-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  animations: [
+    trigger(
+      'valueChanged',
+      [
+          transition('void => *', []),   // when the item is created
+          transition('* => void', []),   // when the item is removed
+          transition('* => *', [         // when the item is changed
+              animate(800, keyframes([  // animate for 1200 ms
+                  style ({ opacity : 0 }),
+                  style ({ opacity : 1 }),
+              ])),
+          ]),
+      ]),
+  ]
 })
-export class HomePageComponent {
+
+export class HomePageComponent implements OnInit{
+  
+  login:boolean = true;
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  toggleLogin(){
+    this.login = !this.login;
+  }
 
 }
